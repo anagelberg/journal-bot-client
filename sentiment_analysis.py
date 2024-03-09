bd01d2bac5212cf71d6c1c24674b718e485c6f2f
Zayce <zayced@gmail.com>
2024-03-09 14:07:39 -0800

    check

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -7,20 +7,20 @@ co = cohere.Client('WGVT0O8pNsZqNvm4RIhJ78uJ0hdkSsAN3il9ufPd')
 examples=[
   Example("The order came 5 days early", "positive review"),
   Example("The item exceeded my expectations", "positive review"),
-  Example("I ordered more for my friends", "positive review"),
-  Example("I would buy this again", "positive review"),
-  Example("I would recommend this to others", "positive review"),
-  Example("The package was damaged", "negative review"),
-  Example("The order is 5 days late", "negative review"),
-  Example("The order was incorrect", "negative review"),
-  Example("I want to return my item", "negative review"),
-  Example("The item's material feels low quality", "negative review"),
-  Example("The product was okay", "neutral review"),
+  # Example("I ordered more for my friends", "positive review"),
+  # Example("I would buy this again", "positive review"),
+  # Example("I would recommend this to others", "positive review"),
+  # Example("The package was damaged", "negative review"),
+  # Example("The order is 5 days late", "negative review"),
+  # Example("The order was incorrect", "negative review"),
+  # Example("I want to return my item", "negative review"),
+  # Example("The item's material feels low quality", "negative review"),
+  # Example("The product was okay", "neutral review"),
   Example("I received five items in total", "neutral review"),
-  Example("I bought it from the website", "neutral review"),
-  Example("I used the product this morning", "neutral review"),
-  Example("The product arrived yesterday", "neutral review"),
-  Example("The product is amazing", "positive review")
+  Example("I bought it from the website", "neutral review")
+  # Example("I used the product this morning", "neutral review"),
+  # Example("The product arrived yesterday", "neutral review"),
+  # Example("The product is amazing", "positive review")
 ]
 inputs=[
   "This item was broken when it arrived"
