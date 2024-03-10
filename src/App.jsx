2b5fc79732c79eb713dccb9c394d2244074e5878
anagelberg <amy.nagelberg@gmail.com>
2024-03-10 05:17:37 -0700

    add routing to sidebar for different dates

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,28 @@ import Day from './pages/Day/Day';
 import './App.scss';
 
 import Sidebar from './components/Sidebar/Sidebar';
+import { Route } from 'react-router-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
-    <div className="app">
-      <h1 className='app__title'>Journal GPT</h1>
-      <div className='app__day'>
-        <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+    <BrowserRouter>
+      <div className="app">
+        <h1 className='app__title'>Journal GPT</h1>
+        <div className='app__day'>
+          <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
 
-        {/* Routes go here */}
-        <Day isSideBarOpen={isOpen} />
+          {/* Routes go here */}
+          <Routes>
+            <Route path='/' element={<Day isSideBarOpen={isOpen} />} />
+            <Route path='/:date' element={<Day isSideBarOpen={isOpen} />} />
+          </Routes>
+        </div>
+
       </div>
-
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Day/Day.jsx
+++ src/pages/Day/Day.jsx
@@ -1,8 +1,18 @@
 import './Day.scss';
 import ChatBox from '../../components/ChatBox/ChatBox';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Day({ isSideBarOpen }) {
+
+    const { date } = useParams();
+
+    /* when date changes, also need to reload the sidebar -- placeholder */
+    useEffect(() => {
+        console.log(date);
+    }, [date])
+
+
 
     return (
         <ChatBox isSideBarOpen={isSideBarOpen} />
