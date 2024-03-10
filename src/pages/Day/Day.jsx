23e58a48435769c85527bb062965f1e91899b793
anagelberg <amy.nagelberg@gmail.com>
2024-03-10 08:51:21 -0700

    add error routing

--- src/pages/Day/Day.jsx
+++ src/pages/Day/Day.jsx
@@ -1,16 +1,21 @@
 import './Day.scss';
 import ChatBox from '../../components/ChatBox/ChatBox';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
-function Day({ isSideBarOpen, setIsTodaySelected, isTodaySelected }) {
+function Day({ isSideBarOpen, setIsTodaySelected, isTodaySelected, dates }) {
 
     const { date } = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         const currentDate = new Date().toISOString().split('T')[0];
         if (date === currentDate) {
             setIsTodaySelected(true);
+        } else if (!date) {
+            navigate(`/${currentDate}`);
+        } else if (!dates.includes(date)) {
+            navigate(`/not-found/404`)
         } else {
             setIsTodaySelected(false);
         }
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import './NotFound.scss';
+
+function NotFound() {
+    return (
+        <div className='not-found'>
+            <h1 className='not-found__title'>404</h1>
+            <p className='not-found__message'>Page not found</p>
+        </div>
+    );
+}
+
+export default NotFound;
