155151d689af523a53fb1151b9ff8e4da0b6f895
anagelberg <amy.nagelberg@gmail.com>
2024-03-12 14:57:11 -0700

    added gif for message loading

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import BotChatBubble from "../BotChatBubble/BotChatBubble";
 import UserChatBubble from "../UserChatBubble/UserChatBubble";
 import JournalEntry from "../JournalEntry/JournalEntry";
 import { ReactComponent as SendIcon } from '../../assets/icons/send.svg';
 import './ChatBox.scss';
 import axios from "axios";
+import loadingGif from '../../assets/messaging.gif';
 
 function ChatBox({ isSideBarOpen, isTodaySelected, date }) {
     //TODO: improve the enter key functionality for better UI
@@ -16,8 +17,8 @@ function ChatBox({ isSideBarOpen, isTodaySelected, date }) {
             role: "CHATBOT",
         }
     ]);
-
-
+    const [messageLoading, setMessageLoading] = useState(false);
+    const scrollRef = useRef(null);
 
 
 
@@ -57,6 +58,12 @@ function ChatBox({ isSideBarOpen, isTodaySelected, date }) {
             }
         ]);
 
+        setMessageLoading(true);
+        setTimeout(() => {
+            scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+        }, 100);
+
+
         try {
             const response = await axios.post(`${import.meta.env.VITE_API_URL}/chat`, {
                 user_message: entryText
@@ -69,6 +76,13 @@ function ChatBox({ isSideBarOpen, isTodaySelected, date }) {
                     role: "CHATBOT"
                 }
             ]);
+
+            if (response) {
+                setMessageLoading(false);
+                setTimeout(() => {
+                    scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+                }, 100);
+            }
         } catch (error) {
             console.error("Error in getting chatbot response:", error);
         }
@@ -86,12 +100,13 @@ function ChatBox({ isSideBarOpen, isTodaySelected, date }) {
                         }
                     })
                 }
+                {messageLoading && <img className="chatbox__loading" src={loadingGif} alt="response message is loading" />}
+                <div ref={scrollRef}></div>
             </div>
 
             {isTodaySelected &&
                 <form className="add-entry-form" onSubmit={(e) => {
                     e.preventDefault();
-                    console.log("Form submitted");
                     handleAddEntry(entryText)
                 }
                 }>
